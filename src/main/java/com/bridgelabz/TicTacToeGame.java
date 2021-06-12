c3380020c7439083c2ccff8c55a94c9e94a8bc68
tushal kumar <tushalkashyap11@gmail.com>
2021-06-12 10:22:45 -0700

    ADDED: allow to choose option x and o

--- src/main/java/com/bridgelabz/TicTacToeGame.java
+++ src/main/java/com/bridgelabz/TicTacToeGame.java
@@ -3,10 +3,30 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TicTacToeGame {
+    public static void chooseOption () {
+
+        char computersSymbol = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("give the  option 'o' or 'x' : ");
+        char option = sc.next().charAt(0);
+
+
+        if ( option=='o') {
+            computersSymbol='x';
+        } else if (option=='x'){
+            computersSymbol='0';
+        } else {
+            System.out.print("invalied option: ");
+        }
+        System.out.println("Players option : "+ option+"\nand computers option is : "+computersSymbol);
+
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the TicTacToe Game");
         Scanner scanner = new Scanner(System.in);
+        chooseOption();
 
         char[][] gameBoard = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
 
