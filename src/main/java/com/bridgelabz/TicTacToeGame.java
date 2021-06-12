cc28ac379cb09f5899bf1cbee8f99d7c0693ff9a
tushal kumar <tushalkashyap11@gmail.com>
2021-06-12 08:02:14 -0700

    ADDED: created an empty TicTacToe Game board

--- src/main/java/com/bridgelabz/TicTacToeGame.java
+++ src/main/java/com/bridgelabz/TicTacToeGame.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+
     public static void main(String[] args) {
         System.out.println("Welcome to the TicTacToe Game");
+        Scanner scanner = new Scanner(System.in);
+
+        char[][] gameBoard = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+
+        System.out.println(gameBoard[0][0] + "|" + gameBoard[0][1] + "|" + gameBoard[0][2]);
+        System.out.println("-+-+-");
+        System.out.println(gameBoard[1][0] + "|" + gameBoard[1][1] + "|" + gameBoard[1][2]);
+        System.out.println("-+-+-");
+        System.out.println(gameBoard[2][0] + "|" + gameBoard[2][1] + "|" + gameBoard[2][2]);
+
     }
 }
